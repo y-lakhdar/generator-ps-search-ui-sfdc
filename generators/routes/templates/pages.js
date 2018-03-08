deb7df356afeeaa57a6a378c3a5e945dfa85e8da
Yassine Lakhdar <ylakhdar@coveo.com>
2018-03-08 18:00:59 -0500

    Update search pages to use search token

--- generators/routes/templates/pages.js
+++ generators/routes/templates/pages.js
@@ -1,59 +1,76 @@
 'use strict';
-var express = require('express');
-var router = express.Router();
-var cfg = require('../config');
+const express = require('express');
+const router = express.Router();
+const cfg = require('../config');
 const passport = require('../passports');
 const middleware = require('../middleware');
 
-router.use((req, res, next)=>{
-    req.filter = cfg.coveo.filter;
-    next();    
+router.use((req, res, next) => {
+  req.filter = cfg.coveo.filter;
+  next();
 });
 
 router.get('/', (req, res) => {
-    res.redirect('/agent-full-search');
+  res.redirect('/agent-full-search');
 });
 
 // unprotected Full Search page
 router.get('/full-search', (req, res) => {
-    res.render('pages/full-search', { 
-        prototypeTitle : 'Full Search Test',
-        config: cfg 
-    });
+  res.render('pages/full-search', {
+    prototypeTitle: 'Full Search Test',
+    config: cfg
+  });
 });
 
 // Pilot Search [Protected + generatedSearchToken]
 router.get('/pilot-search', passport.protected, middleware.ensureTokenGenerated, (req, res) => {
-
-    res.render('pages/pilot-search', { 
-        prototypeTitle : 'Pilot Search',
-        config: cfg,
-        token: req.session.tokens[req.originalUrl]
-    });
+  res.render('pages/pilot-search', {
+    prototypeTitle: 'Pilot Search',
+    config: cfg,
+    token: req.session.tokens[req.originalUrl]
+  });
 });
 
 // Community Search
 router.get('/community-search', (req, res) => {
-    res.render('pages/community-search', { 
-        prototypeTitle : 'Community Search',
-        config: cfg 
-    });
+  coveoPlatformApi
+    .getSearchToken()
+    .then(data => {
+      res.render('pages/community-search', {
+        prototypeTitle: 'Community Search Prototype',
+        config: cfg,
+        token: data.token
+      });
+    })
+    .catch(err => console.error(err));
 });
 
 // SFDC Agent Box
 router.get('/agent-box', (req, res) => {
-    res.render('pages/agent-box', { 
-        prototypeTitle : 'Agent Insight Panel',
-        config: cfg
-    });
+  coveoPlatformApi
+    .getSearchToken()
+    .then(data => {
+      res.render('pages/agent-box', {
+        prototypeTitle: 'Agent Insight Panel',
+        config: cfg,
+        token: data.token
+      });
+    })
+    .catch(err => console.error(err));
 });
 
 // SFDC Agent Full
 router.get('/agent-full-search', (req, res) => {
-    res.render('pages/agent-full-search', { 
-        prototypeTitle : 'Agent Full Search',
-        config: cfg
-    });
+  coveoPlatformApi
+    .getSearchToken()
+    .then(data => {
+      res.render('pages/agent-full-search', {
+        prototypeTitle: 'Agent Full Search',
+        config: cfg,
+        token: data.token
+      });
+    })
+    .catch(err => console.error(err));
 });
 
 module.exports = router;
